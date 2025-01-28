d7b4a2e4b301f6c0378dfcf4337898d1dd51c2c4
shenlehan <shenlehan233@gmail.com>
2025-01-28 15:12:00 +0800

    finish hw 08

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,7 +1,33 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+    (if (null? s) #t
+        (if (null? (cdr s)) #t
+            (if (> (car s) (car (cdr s))) #f
+                (ascending? (cdr s))
+            )
+        )
+    )
+)
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (if (null? s) nil
+        (if (pred (car s)) (cons (car s) (my-filter pred (cdr s)))
+            (my-filter pred (cdr s))
+        )
+    )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (cond
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else
+            (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))) ; use cons recursively to make sure there is only one pair of paranthese
+        )
+    )
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+    (if (null? s) nil
+        (cons (car s) (no-repeats (my-filter (lambda (x) (not (= x (car s)))) (cdr s))))
+    )
+)
