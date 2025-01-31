ef2f6d949a3e953d77da50faa5625a72bdcd3087
shenlehan <shenlehan233@gmail.com>
2025-01-31 15:46:52 +0800

    finish lab 05-07

--- labs/lab07/lab07.py
+++ labs/lab07/lab07.py
@@ -1,133 +1,191 @@
-def amplify(f, x):
-    """Yield the longest sequence x, f(x), f(f(x)), ... that are all true values
+class Account:
+    """An account has a balance and a holder.
+
+    >>> a = Account('John')
+    >>> a.deposit(10)
+    10
+    >>> a.balance
+    10
+    >>> a.interest
+    0.02
+    >>> a.time_to_retire(10.25)  # 10 -> 10.2 -> 10.404
+    2
+    >>> a.balance                # Calling time_to_retire method should not change the balance
+    10
+    >>> a.time_to_retire(11)     # 10 -> 10.2 -> ... -> 11.040808032
+    5
+    >>> a.time_to_retire(100)
+    117
+    """
+    max_withdrawal = 10
+    interest = 0.02
+
+    def __init__(self, account_holder):
+        self.balance = 0
+        self.holder = account_holder
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient funds"
+        if amount > self.max_withdrawal:
+            return "Can't withdraw that amount"
+        self.balance = self.balance - amount
+        return self.balance
+
+    def time_to_retire(self, amount):
+        """Return the number of years until balance would grow to amount."""
+        assert self.balance > 0 and amount > 0 and self.interest > 0
+        "*** YOUR CODE HERE ***"
+        res, cur = 1, self.balance
+        while cur * (self.interest + 1) < amount:
+            res += 1
+            cur *= (self.interest + 1)
+        return res
+
+
+class FreeChecking(Account):
+    """A bank account that charges for withdrawals, but the first two are free!
+
+    >>> ch = FreeChecking('Jack')
+    >>> ch.balance = 20
+    >>> ch.withdraw(100)  # First one's free. Still counts as a free withdrawal even though it was unsuccessful
+    'Insufficient funds'
+    >>> ch.withdraw(3)    # Second withdrawal is also free
+    17
+    >>> ch.balance
+    17
+    >>> ch.withdraw(3)    # Now there is a fee because free_withdrawals is only 2
+    13
+    >>> ch.withdraw(3)
+    9
+    >>> ch2 = FreeChecking('John')
+    >>> ch2.balance = 10
+    >>> ch2.withdraw(3) # No fee
+    7
+    >>> ch.withdraw(3)  # ch still charges a fee
+    5
+    >>> ch.withdraw(5)  # Not enough to cover fee + withdraw
+    'Insufficient funds'
+    """
+    withdraw_fee = 1
+    free_withdrawals = 2
+
+    def __init__(self, account_holder):
+        super().__init__(account_holder)
     
-    >>> list(amplify(lambda s: s[1:], 'boxes'))
-    ['boxes', 'oxes', 'xes', 'es', 's']
-    >>> list(amplify(lambda x: x//2-1, 14))
-    [14, 6, 2]
+    def withdraw(self, amount):
+        self.free_withdrawals -= 1
+        if self.free_withdrawals >= 0:
+            if self.balance < amount:
+                return 'Insufficient funds'
+            elif amount > self.max_withdrawal:
+                return "Can't withdraw that amount"
+            else:
+                self.balance = self.balance - amount
+                return self.balance
+        else:
+            if self.balance < amount + self.withdraw_fee:
+                return 'Insufficient funds'
+            elif amount > self.max_withdrawal:
+                return "Can't withdraw that amount"
+            else:
+                self.balance = self.balance - amount - self.withdraw_fee
+                return self.balance
+
+def without(s, i):
+    """Return a new linked list like s but without the element at index i.
+
+    >>> s = Link(3, Link(5, Link(7, Link(9))))
+    >>> without(s, 0)
+    Link(5, Link(7, Link(9)))
+    >>> without(s, 2)
+    Link(3, Link(5, Link(9)))
+    >>> without(s, 4)           # There is no index 4, so all of s is retained.
+    Link(3, Link(5, Link(7, Link(9))))
+    >>> without(s, 4) is not s  # Make sure a copy is created
+    True
     """
     "*** YOUR CODE HERE ***"
+    def solve(cur, pos):
+        if cur == Link.empty:
+            return cur
+        if pos == i:
+            return cur.rest
+        else:
+            return Link(cur.first, solve(cur.rest, pos + 1))
+    return solve(s, 0)
 
 
-class Person:
-    """Person class.
+def duplicate_link(s, val):
+    """Mutates s so that each element equal to val is followed by another val.
 
-    >>> steven = Person("Steven")
-    >>> steven.repeat()       # initialized person has the below starting repeat phrase!
-    'I squirreled it away before it could catch on fire.'
-    >>> steven.say("Hello")
-    'Hello'
-    >>> steven.repeat()
-    'Hello'
-    >>> steven.greet()
-    'Hello, my name is Steven'
-    >>> steven.repeat()
-    'Hello, my name is Steven'
-    >>> steven.ask("preserve abstraction barriers")
-    'Would you please preserve abstraction barriers'
-    >>> steven.repeat()
-    'Would you please preserve abstraction barriers'
+    >>> x = Link(5, Link(4, Link(5)))
+    >>> duplicate_link(x, 5)
+    >>> x
+    Link(5, Link(5, Link(4, Link(5, Link(5)))))
+    >>> y = Link(2, Link(4, Link(6, Link(8))))
+    >>> duplicate_link(y, 10)
+    >>> y
+    Link(2, Link(4, Link(6, Link(8))))
+    >>> z = Link(1, Link(2, (Link(2, Link(3)))))
+    >>> duplicate_link(z, 2) # ensures that back to back links with val are both duplicated
+    >>> z
+    Link(1, Link(2, Link(2, Link(2, Link(2, Link(3))))))
     """
+    "*** YOUR CODE HERE ***"
+    def solve(cur):
+        if cur == Link.empty:
+            return
+        if cur.first == val:
+            cur.rest = Link(val, cur.rest)
+            solve(cur.rest.rest)
+        else:
+            solve(cur.rest)
 
-    def __init__(self, name):
-        self.name = name
-        "*** YOUR CODE HERE ***"
+    solve(s)
 
-    def say(self, stuff):
-        "*** YOUR CODE HERE ***"
-        return stuff
+class Link:
+    """A linked list.
 
-    def ask(self, stuff):
-        return self.say("Would you please " + stuff)
-
-    def greet(self):
-        return self.say("Hello, my name is " + self.name)
-
-    def repeat(self):
-        "*** YOUR CODE HERE ***"
-
-
-class SmartFridge:
-    """"
-    >>> fridgey = SmartFridge()
-    >>> fridgey.add_item('Mayo', 1)
-    'I now have 1 Mayo'
-    >>> fridgey.add_item('Mayo', 2)
-    'I now have 3 Mayo'
-    >>> fridgey.use_item('Mayo', 2.5)
-    'I have 0.5 Mayo left'
-    >>> fridgey.use_item('Mayo', 0.5)
-    'Oh no, we need more Mayo!'
-    >>> fridgey.add_item('Eggs', 12)
-    'I now have 12 Eggs'
-    >>> fridgey.use_item('Eggs', 15)
-    'Oh no, we need more Eggs!'
-    >>> fridgey.add_item('Eggs', 1)
-    'I now have 1 Eggs'
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
     """
+    empty = ()
 
-    def __init__(self):
-        self.items = {}
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
 
-    def add_item(self, item, quantity):
-        "*** YOUR CODE HERE ***"
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
 
-    def use_item(self, item, quantity):
-        "*** YOUR CODE HERE ***"
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
 
-
-class CucumberGame:
-    """Play a round and return all winners so far. Cards is a list of pairs.
-    Each (who, card) pair in cards indicates who plays and what card they play.
-    >>> g = CucumberGame()
-    >>> g.play_round(3, [(3, 4), (0, 8), (1, 8), (2, 5)])
-    >>> g.winners
-    [1]
-    >>> g.play_round(1, [(3, 5), (1, 4), (2, 5), (0, 8), (3, 7), (0, 6), (1, 7)])
-    It is not your turn, player 3
-    It is not your turn, player 0
-    The round is over, player 1
-    >>> g.winners
-    [1, 3]
-    >>> g.play_round(3, [(3, 7), (2, 5), (0, 9)]) # Round is never completed
-    It is not your turn, player 2
-    >>> g.winners
-    [1, 3]
-    """
-
-    def __init__(self):
-        self.winners = []
-
-    def play_round(self, starter, cards):
-        r = Round(starter)
-        for who, card in cards:
-            try:
-                r.play(who, card)
-            except AssertionError as e:
-                print(e)
-        if r.winner != None:
-            self.winners.append(r.winner)
-
-
-class Round:
-    players = 4
-
-    def __init__(self, starter):
-        self.starter = starter
-        self.next_player = starter
-        self.highest = -1
-        self.winner = None
-
-    def play(self, who, card):
-        assert not self.is_complete(), f'The round is over, player {who}'
-        assert who == self.next_player, f'It is not your turn, player {who}'
-        self.next_player = ______________________________________
-        if card >= self.highest:
-            ______________________________________
-            ______________________________________
-        if ______________________________________:
-            ______________________________________
-
-    def is_complete(self):
-        """ Checks if a game could end. """
-        return ______________________________________
